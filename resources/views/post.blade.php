49d10dca03cc3b8527407b448ba346908817550c
Wildanrazzan <wildanrazzan82@gmail.com>
2025-08-14 15:25:44 +0700

    feat: implement blog functionality with dynamic posts and author details

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,16 +1,14 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
 
-    @foreach ($posts as $post)
-    <article class="py-8 max-w-screen-md border-b border-gray-300">
+    <article class="py-8 max-w-screen-md">
         <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
         <div class="text-base text-gray-500">
             <a href="#">{{ $post['author'] }}</a> | 17 Agustus 2025
         </div>
         <p class="my-4 font-light">
-            {{ Str::limit($post['body'], 100) }}
+            {{$post['body']}}
         </p>
-        <a href="/blog/{{ $post['slug'] }}" class="text-blue-500 hover:underline">Read more</a>
+        <a href="/blog" class="text-blue-500 hover:underline">Back to Blog</a>
     </article>
-    @endforeach
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,13 +8,30 @@ Route::get('/', function () {
 });
 
 Route::get('/about', function () {
-    return view('about', ['nama' => 'Willy', 'title'=> 'About']);
+    return view('about', ['title' => 'About']);
 });
 
 Route::get('/blog', function () {
-    return view('blog' ,['title'=> 'Blog']);
+    return view('blog' ,['title' => 'Blog', 'posts' => [
+        ['id' => '1','slug' => 'judul-artikel-1','title' => 'Judul Artikel 1', 'author' => 'Muhamad Wildan Razzan', 'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, molestiae! Explicabo atque praesentium saepe, quod vitae quo id quasi cum recusandae sed eius, ducimus commodi cupiditate voluptatum. Nobis, quae soluta.'],
+        ['id' => '2','slug' => 'judul-artikel-2','title' => 'Judul Artikel 2', 'author' => 'Muhamad Wildan Razzan', 'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, molestiae! Explicabo atque praesentium saepe, quod vitae quo id quasi cum recusandae sed eius, ducimus commodi cupiditate voluptatum. Nobis, quae soluta.'],
+        ['id' => '3','slug' => 'judul-artikel-3','title' => 'Judul Artikel 3', 'author' => 'Muhamad Wildan Razzan', 'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, molestiae! Explicabo atque praesentium saepe, quod vitae quo id quasi cum recusandae sed eius, ducimus commodi cupiditate voluptatum. Nobis, quae soluta.'],
+    ]]);
+});
+
+Route::get('/blog/{slug}', function ($slug) {
+    $posts = [
+        ['id' => '1','slug' => 'judul-artikel-1','title' => 'Judul Artikel 1', 'author' => 'Muhamad Wildan Razzan', 'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, molestiae! Explicabo atque praesentium saepe, quod vitae quo id quasi cum recusandae sed eius, ducimus commodi cupiditate voluptatum. Nobis, quae soluta.'],
+        ['id' => '2','slug' => 'judul-artikel-2','title' => 'Judul Artikel 2', 'author' => 'Muhamad Wildan Razzan', 'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, molestiae! Explicabo atque praesentium saepe, quod vitae quo id quasi cum recusandae sed eius, ducimus commodi cupiditate voluptatum. Nobis, quae soluta.'],
+        ['id' => '3','slug' => 'judul-artikel-3','title' => 'Judul Artikel 3', 'author' => 'Muhamad Wildan Razzan', 'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, molestiae! Explicabo atque praesentium saepe, quod vitae quo id quasi cum recusandae sed eius, ducimus commodi cupiditate voluptatum. Nobis, quae soluta.'],
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
-    return view('contact',['email' => 'wildanrazzan82@gmail.com','telp' => '083830837156', 'title'=> 'Contact']);
+    return view('contact',['title' => 'Contact']);
 });
